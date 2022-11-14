2da49dcd3d15d6e32494a2a8997da5e941606974
cosmo-18 <cosimo.simonesolfrizzi@gmail.com>
2022-11-14 04:23:49 +0100

    finire di implementare scorrimento

---
+++ assets/script/main.js
@@ -0,0 +1,17 @@
+// Iinzio sintassi vue
+
+var app = new Vue (
+    {
+        el: '#root',
+        data: {
+                img1:'https://i.picsum.photos/id/342/200/200.jpg?hmac=RWvP86WrI79J1lVaj-tfUFqvBHgAWnsRKVI9ER9Hdzc',
+                img2:'https://i.picsum.photos/id/981/200/200.jpg?hmac=LRIdIKhx3zHhDGgKVGHBdRjNRrgVbpWUw02O7Uuy2C4', 
+                img3:'https://i.picsum.photos/id/1072/200/200.jpg?hmac=jar1_w-0fdzV_d70mmk9VzkBQA9B_oSZuZs21we31ZE', 
+                img4:'https://i.picsum.photos/id/679/200/200.jpg?hmac=sPsw4YJPQkWFqo2k5UycejGhY4UXvaDXStGmvJEhFBA',
+                img5:'https://i.picsum.photos/id/361/200/200.jpg?hmac=8pPTUqe61Cxj4FYarGS9vZKtqUSjAzxOQJ0zBIHq28o'   
+        },
+        methods:{ 
+            
+        }
+    }
+)
