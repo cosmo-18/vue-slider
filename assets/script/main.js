8743839eced561364a131891278b555bc8ac7a96
cosmo-18 <cosimo.simonesolfrizzi@gmail.com>
2022-11-15 02:58:32 +0100

    fixata la funzione di scorrimento

--- assets/script/main.js
+++ assets/script/main.js
@@ -3,15 +3,44 @@
 var app = new Vue (
     {
         el: '#root',
-        data: {
-                img1:'https://i.picsum.photos/id/342/200/200.jpg?hmac=RWvP86WrI79J1lVaj-tfUFqvBHgAWnsRKVI9ER9Hdzc',
-                img2:'https://i.picsum.photos/id/981/200/200.jpg?hmac=LRIdIKhx3zHhDGgKVGHBdRjNRrgVbpWUw02O7Uuy2C4', 
-                img3:'https://i.picsum.photos/id/1072/200/200.jpg?hmac=jar1_w-0fdzV_d70mmk9VzkBQA9B_oSZuZs21we31ZE', 
-                img4:'https://i.picsum.photos/id/679/200/200.jpg?hmac=sPsw4YJPQkWFqo2k5UycejGhY4UXvaDXStGmvJEhFBA',
-                img5:'https://i.picsum.photos/id/361/200/200.jpg?hmac=8pPTUqe61Cxj4FYarGS9vZKtqUSjAzxOQJ0zBIHq28o'   
-        },
-        methods:{ 
-            
-        }
-    }
-)
+        data:{
+            indexImg: 0,
+            obj: [
+                {
+                    img:'https://i.picsum.photos/id/456/200/200.jpg?hmac=Nu8ETZuaEwOyl13MjrpOo92an166yAu4I2emrU09uxg'
+                },
+                {
+                    img:'https://i.picsum.photos/id/981/200/200.jpg?hmac=LRIdIKhx3zHhDGgKVGHBdRjNRrgVbpWUw02O7Uuy2C4'
+                },
+                {
+                    img:'https://i.picsum.photos/id/1072/200/200.jpg?hmac=jar1_w-0fdzV_d70mmk9VzkBQA9B_oSZuZs21we31ZE'
+                },
+                {
+                    img:'https://i.picsum.photos/id/679/200/200.jpg?hmac=sPsw4YJPQkWFqo2k5UycejGhY4UXvaDXStGmvJEhFBA'
+                },
+                {
+                    img:'https://i.picsum.photos/id/361/200/200.jpg?hmac=8pPTUqe61Cxj4FYarGS9vZKtqUSjAzxOQJ0zBIHq28o'
+                }
+                       ] 
+             },
+             methods: {
+                nextImg: function(){
+
+                    if (this.indexImg < this.obj.length-1) {
+                        this.indexImg++;  
+                    } else {
+                        this.indexImg=0;
+                    }
+                },
+
+                beforeImg: function (){
+                    if (this.indexImg <= this.obj.length) {
+                        this.indexImg--;
+                    } else {
+
+                    }
+                }
+
+             }
+
+    })
